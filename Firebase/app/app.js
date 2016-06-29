cea5bada9d57e87e359027cdd1254be3a9f171a5
l4u532 <kl@shipbeat.com>
2016-06-29 15:38:43 +0200

    First commit

--- Firebase/app/app.js
+++ Firebase/app/app.js
@@ -3,6 +3,18 @@ var application = require("application");
 // added this here so we can do some wiring
 var firebase = require("nativescript-plugin-firebase");
 
+  firebase.init({
+    url: 'https://mercury-f8270.firebaseio.com', // add your own endpoint
+    persist: true // Persist data to disk. Default false.
+  }).then(
+      function (result) {
+        console.log("firebase.init done");
+      },
+      function (error) {
+        console.log("firebase.init error: " + error);
+      }
+  );
+
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
 application.start();
